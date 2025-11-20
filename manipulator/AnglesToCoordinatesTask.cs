d20c1452f7f308c65c451820f6c4e977936ffcfa
Kritt07 <dum98415@gmail.com>
2025-11-20 22:14:08 +0500

    Выполнил задание Манипулятор

--- manipulator/AnglesToCoordinatesTask.cs
+++ manipulator/AnglesToCoordinatesTask.cs
@@ -1,42 +1,56 @@
 using System;
 using Avalonia;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using static Manipulation.Manipulator;
 
 namespace Manipulation;
 
 public static class AnglesToCoordinatesTask
 {
-	/// <summary>
-	/// По значению углов суставов возвращает массив координат суставов
-	/// в порядке new []{elbow, wrist, palmEnd}
-	/// </summary>
-	public static Point[] GetJointPositions(double shoulder, double elbow, double wrist)
-	{
-		var elbowPos = new Point(0, UpperArm);
-		var wristPos = new Point(Forearm, UpperArm);
-		var palmEndPos = new Point((Forearm + Palm), UpperArm);
-		return new[]
-		{
-			elbowPos,
-			wristPos,
-			palmEndPos
-		};
-	}
+    public static Point[] GetJointPositions(double shoulder, double elbow, double wrist)
+    {
+		var angle1 = shoulder;
+		var angle2 = angle1 + elbow - Math.PI;
+		var angle3 = angle2 + wrist - Math.PI;
+
+        var elbowPos = GetNextPoint(angle1, UpperArm, new Point(0, 0));
+        var wristPos = GetNextPoint(angle2, Forearm, elbowPos);
+        var palmEndPos = GetNextPoint(angle3, Palm, wristPos);
+        return new [] { elbowPos, wristPos, palmEndPos };
+    }
+
+    public static Point GetNextPoint(double angle, float length, Point startPoint)
+    {
+        var x = (float)Math.Cos(angle) * length + startPoint.X;
+        var y = (float)Math.Sin(angle) * length + startPoint.Y;
+        return new Point(x, y);
+    }
 }
 
 [TestFixture]
 public class AnglesToCoordinatesTask_Tests
 {
-	// Доработайте эти тесты!
-	// С помощью строчки TestCase можно добавлять новые тестовые данные.
-	// Аргументы TestCase превратятся в аргументы метода.
-	[TestCase(Math.PI / 2, Math.PI / 2, Math.PI, Forearm + Palm, UpperArm)]
-	public void TestGetJointPositions(double shoulder, double elbow, double wrist, double palmEndX, double palmEndY)
-	{
-		var joints = AnglesToCoordinatesTask.GetJointPositions(shoulder, elbow, wrist);
-		Assert.AreEqual(palmEndX, joints[2].X, 1e-5, "palm endX");
-		Assert.AreEqual(palmEndY, joints[2].Y, 1e-5, "palm endY");
-		Assert.Fail("TODO: проверить, что расстояния между суставами равны длинам сегментов манипулятора!");
-	}
+    [TestCase(Math.PI / 2, Math.PI, 3 * Math.PI, 0, Forearm + UpperArm + Palm)] 
+    [TestCase(Math.PI / 2, Math.PI / 2, Math.PI / 2, Forearm, UpperArm - Palm)] 
+    [TestCase(Math.PI / 2, Math.PI / 2, Math.PI, Forearm + Palm, UpperArm)]     
+    [TestCase(Math.PI / 2, 3 * Math.PI / 2, 3 * Math.PI / 2, -Forearm, UpperArm - Palm)] 
+
+    public void TestGetJointPositions(double shoulder, double elbow, double wrist, double palmEndX, double palmEndY)
+    {
+        var joints = AnglesToCoordinatesTask.GetJointPositions(shoulder, elbow, wrist);
+        
+        Assert.That(joints[2].X, Is.EqualTo(palmEndX).Within(1e-5), "palm endX");
+        Assert.That(joints[2].Y, Is.EqualTo(palmEndY).Within(1e-5), "palm endY");
+        Assert.That(GetDistance(joints[0], new Point(0, 0)), Is.EqualTo(UpperArm).Within(1e-5));
+        Assert.That(GetDistance(joints[0], joints[1]), Is.EqualTo(Forearm).Within(1e-5));
+        Assert.That(GetDistance(joints[1], joints[2]), Is.EqualTo(Palm).Within(1e-5));
+    }
+
+    public float GetDistance(Point a, Point b)
+    {
+        var differenceX = (a.X - b.X) * (a.X - b.X);
+        var differenceY = (a.Y - b.Y) * (a.Y - b.Y);
+        return (float)Math.Sqrt(differenceX + differenceY);
+    }
 }
--- manipulator/TriangleTask.cs
+++ manipulator/TriangleTask.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 
 namespace Manipulation;
 
@@ -10,7 +11,31 @@ public class TriangleTask
     /// </summary>
     public static double GetABAngle(double a, double b, double c)
     {
-        return 0.0;
+        // Проверка на отрицательные длины и нулевые стороны
+        if (a < 0 || b < 0 || c < 0)
+        {
+            return double.NaN;
+        }
+        
+        // Проверка на вырожденный треугольник (нулевые стороны)
+        if (a == 0 || b == 0)
+        {
+            // Если обе стороны нулевые и третья тоже - особый вырожденный случай
+            if (a == 0 && b == 0 && c == 0)
+                return 0; // Все точки совпадают - угол 0 градусов
+            else if (a == 0 && b == 0)
+                return double.NaN; // Невозможно определить угол
+            
+            // Если одна из сторон нулевая
+            if (a == 0 && b > 0 && c == b) return 0; // Вырожденный в отрезок
+            if (b == 0 && a > 0 && c == a) return 0; // Вырожденный в отрезок
+            
+            return double.NaN;
+        }
+
+        // нахождение угла при помощи теоремы косинусов
+        var angle = Math.Acos((a * a + b * b - c * c) / (2 * a * b));
+        return angle;
     }
 }
 
@@ -19,9 +44,26 @@ public class TriangleTask_Tests
 {
     [TestCase(3, 4, 5, Math.PI / 2)]
     [TestCase(1, 1, 1, Math.PI / 3)]
-    // добавьте ещё тестовых случаев!
+    [TestCase(150, 120, 60, 0.3897607327974747)]
+    [TestCase(3, 4, 5, 1.5707963267948966)]
+    [TestCase(1, 1, 2, Math.PI)]
+    [TestCase(2, 1, 1, 0)]
+    [TestCase(1, 2, 1, 0)]
+    [TestCase(1, 1, 2.001, double.NaN)]
+    [TestCase(1, 2.001, 1, double.NaN)]
+    [TestCase(2.001, 1, 1, double.NaN)]
+    [TestCase(0, 5, 5, double.NaN)]
+    [TestCase(5, 0, 5, double.NaN)]
+    [TestCase(5, 5, 0, 0)]
+    [TestCase(-3, -2, -4, double.NaN)]
+    [TestCase(3, 4, 5, 1.5707963267948966d)]
+    [TestCase(1, 1, 2, 3.141592653589793d)]
     public void TestGetABAngle(double a, double b, double c, double expectedAngle)
     {
-        Assert.Fail("Not implemented yet");
+        var actual = TriangleTask.GetABAngle(a, b, c);
+        if (double.IsNaN(expectedAngle))
+            Assert.That(actual, Is.NaN);
+        else
+            ClassicAssert.AreEqual(expectedAngle, actual, 1e-10);
     }
 }

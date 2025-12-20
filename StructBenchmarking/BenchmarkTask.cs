c1ff98cb72c77d46d17de15dc4ed8d4d62000201
Kritt07 <dum98415@gmail.com>
2025-12-20 15:42:19 +0500

    практика Benchmark сделана

--- StructBenchmarking/BenchmarkTask.cs
+++ StructBenchmarking/BenchmarkTask.cs
@@ -1,19 +1,52 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 
 namespace StructBenchmarking;
+
 public class Benchmark : IBenchmark
 {
     public double MeasureDurationInMs(ITask task, int repetitionCount)
     {
-        GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
-        GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
-                                        // и как-то повлияет на них.
-           
-		throw new NotImplementedException();
-	}
+        // Принудительный вызов GC для минимизации его влияния на измерения
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        task.Run();
+
+        var stopwatch = Stopwatch.StartNew();
+        for (int i = 0; i < repetitionCount; i++)
+        {
+            task.Run();
+        }
+
+        stopwatch.Stop();
+        return (double)stopwatch.ElapsedMilliseconds / repetitionCount;
+    }
+}
+
+public class StringBuilderTask : ITask
+{
+    public void Run()
+    {
+        var builder = new StringBuilder();
+        for (int i = 0; i < 10000; i++)
+        {
+            builder.Append('a');
+        }
+        string result = builder.ToString();
+    }
+}
+
+public class StringConstructorTask : ITask
+{
+    public void Run()
+    {
+        string result = new string('a', 10000);
+    }
 }
 
 [TestFixture]
@@ -22,6 +55,17 @@ public class RealBenchmarkUsageSample
     [Test]
     public void StringConstructorFasterThanStringBuilder()
     {
-        throw new NotImplementedException();
+        var benchmark = new Benchmark();
+
+        var stringBuilderTask = new StringBuilderTask();
+        var stringConstructorTask = new StringConstructorTask();
+
+        int repetitionCount = 10000;
+        double stringBuilderTime = benchmark.MeasureDurationInMs(stringBuilderTask, repetitionCount);
+        double stringConstructorTime = benchmark.MeasureDurationInMs(stringConstructorTask, repetitionCount);
+
+        ClassicAssert.Less(stringConstructorTime, stringBuilderTime,
+            $"String constructor time: {stringConstructorTime} ms, " +
+            $"StringBuilder time: {stringBuilderTime} ms");
     }
 }
